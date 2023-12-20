1ef6594d31c76b591d6f222a300e9734850a0632
Dmitry MUZYCHENKA <dzmitry.muzychenka@cherishdev.com>
2023-12-20 22:48:31 +0300

    feat: add favicon to head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,17 @@ import svgLoader from 'vite-svg-loader'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'icon',
+                    href: 'favicon.ico',
+                    sizes: 'any'
+                }
+            ]
+        }
+    },
     imports: {
         dirs: ['stores']
     },
