aaed6dba068bc1437ead5332ce304b0bed2ca3e5
Dmitry MUZYCHENKA <dzmitry.muzychenka@cherishdev.com>
2023-12-20 22:38:37 +0300

    fix: change port & pm2 config extension

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'Muzychenka',
-            port: '4000',
+            port: '5000',
             exec_mode: 'cluster',
             instances: 'max',
             script: './.output/server/index.mjs'
