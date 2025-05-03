704abf1b829ba48349ae4c64e9430694d700d6d3
Dzmitry Muzychenka <dzmitry@MacBook-Pro-Dzmitry.local>
2025-05-03 15:37:43 +0300

    feat: add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,40 +2,52 @@ import svgLoader from 'vite-svg-loader'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+    devtools: { enabled: true },
 
-  app: {
-      head: {
-          link: [
-              {
-                  rel: 'icon',
-                  href: '/favicon.ico',
-                  sizes: 'any'
-              }
-          ]
-      }
-  },
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'icon',
+                    href: '/favicon.ico',
+                    sizes: 'any'
+                }
+            ],
+            meta: [
+                {
+                    name: 'description',
+                    content:
+                        'Frontend Developer specializing in TypeScript, JavaScript, Vue, Nuxt, and SolidJS. Building modern, performant web applications with clean code and best practices.'
+                },
+                {
+                    name: 'keywords',
+                    content:
+                        'Frontend Developer, TypeScript Developer, JavaScript Developer, Vue.js Developer, Nuxt.js Developer, SolidJS Developer, Web Development, SPA Development, Modern Frontend, Responsive Web Apps, Dzmitry Muzychenka'
+                }
+            ]
+        }
+    },
 
-  imports: {
-      dirs: ['stores']
-  },
+    imports: {
+        dirs: ['stores']
+    },
 
-  css: ['@/assets/scss/main.scss'],
+    css: ['@/assets/scss/main.scss'],
 
-  vite: {
-      css: {
-          preprocessorOptions: {
-              scss: {
-                  additionalData: `
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
                       @use '@/assets/scss/variables.scss' as *;
                       @use '@/assets/scss/screens.scss' as *;
                   `
-              }
-          }
-      },
-      plugins: [svgLoader()]
-  },
+                }
+            }
+        },
+        plugins: [svgLoader()]
+    },
 
-  modules: ['@vueuse/nuxt', '@nuxtjs/eslint-module'],
-  compatibilityDate: '2025-04-05'
+    modules: ['@vueuse/nuxt', '@nuxtjs/eslint-module'],
+    compatibilityDate: '2025-04-05'
 })
